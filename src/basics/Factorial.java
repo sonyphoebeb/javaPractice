94b7ef55df8c88a6ff398d7bf7df844b9ba91c45
sonyphoebeb <sonyphoebebaree@gmail.com>
2026-03-28 23:21:13 +0530

    feat(basics): add factorial practice program

---
+++ src/basics/Factorial.java
@@ -0,0 +1,21 @@
+package basics;
+
+import java.util.*;
+
+public class Factorial {
+    public static void main(String[] args) {
+
+        System.out.print("Enter a number: ");
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int fact = 1;
+
+        for (int i = 1; i <= n; i++) {
+            fact = fact * i;
+        }
+
+        System.out.println(fact);
+        sc.close();
+    }
+}
