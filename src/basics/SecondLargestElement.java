1ec1798f0a9c62bfb5d35e8542dbd8ecf93f2c0b
sonyphoebeb <sonyphoebebaree@gmail.com>
2026-03-29 16:35:13 +0530

    feat: implement second largest element logic using single pass comparison

---
+++ src/basics/SecondLargestElement.java
@@ -0,0 +1,21 @@
+package basics;
+
+public class SecondLargestElement {
+    public static void main(String[] args) {
+        
+        int[] arr = {1, 2, 3, 4, 5};
+        int largest = arr[0];
+        int secondLargest = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > largest) {
+                secondLargest = largest;
+                largest = arr[i];
+            } else if (arr[i] > secondLargest && arr[i] != largest) {
+                secondLargest = arr[i];
+            }
+        }
+
+        System.out.println(secondLargest);
+    }
+}
